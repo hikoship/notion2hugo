635dc721238eaf3752b57be06f4fa2fb774c891d
hikoship <gao0927@gmail.com>
2023-03-21 04:41:01 -0400

    parse heading

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+console.log('hgao')
 const { Client } = require("@notionhq/client")
 require('dotenv').config()
 const fs = require('fs');
 
 const notion = new Client({ auth: process.env.NOTION_KEY })
 const databaseId = process.env.NOTION_DATABASE_ID
-console.log('hgao')
 
 async function fetch() {
   try {
@@ -35,7 +35,7 @@ function createMd(page, blocks) {
     file_name = page.properties.Filename.rich_text[0].plain_text + ".md"
     addHeader(file_name, page.properties)
     addContent(file_name, blocks.results)
-    
+
     // console.log(page.properties.Name.title[0].plain_text)
     // console.log(blocks)
     // const blockType = blocks.results[0]
@@ -71,11 +71,6 @@ function parseBlock(block) {
   if (block.type.startsWith("heading")) {
     return getHeading(block)
   }
-  switch (block.type) {
-    case "heading_1":
-      // console.log(block.heading_1)
-      return ""
-  }
   return ""
 }
 
@@ -99,6 +94,10 @@ function getTitle(name) {
 }
 
 function getHeading(block) {
+  const type = block.type
+  const headingSize = parseInt(type[type.length - 1])
+console.log(headingSize)
+  return "#".repeat(headingSize) + " " + block[type].rich_text[0].plain_text + '\n'
 
 }
 
