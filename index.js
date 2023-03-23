8f9f9d3768930d08543367f213a0fe664aa581d0
hikoship <gao0927@gmail.com>
2023-03-23 03:19:12 -0400

    parse list

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
 console.log('hgao')
 const { Client } = require("@notionhq/client")
 require('dotenv').config()
-const fs = require('fs');
+const fs = require('fs').promises;
+
+const debug_mode = true
 
 const notion = new Client({ auth: process.env.NOTION_KEY })
 const databaseId = process.env.NOTION_DATABASE_ID
+const post_dir = debug_mode ? 'debug_posts' : process.env.POST_DIR
+const temp_post_dir = debug_mode ? 'debug_posts' : process.env.TEMP_POST_DIR
+
+let prev_block_is_list = false
+
 
 async function fetch() {
-  try {
-    const notion = new Client({ auth: process.env.NOTION_KEY })
-    const database = await notion.databases.query({
-      database_id: databaseId,
-        //   filter: {
-        //     // property: "Landmark",
-        //     // rich_text: {
-        //     //   contains: "Bridge",
-        //     // },
-    })
+  const notion = new Client({ auth: process.env.NOTION_KEY })
+  const database = await notion.databases.query({
+    database_id: databaseId,
+      //   filter: {
+      //     // property: "Landmark",
+      //     // rich_text: {
+      //     //   contains: "Bridge",
+      //     // },
+  })
     // for (let i = 0; i < database.results.length; i++) {
-    for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < 1; i++) {
+    try {
       const page = database.results[i]
       const blocks = await notion.blocks.children.list({
         block_id: page.id
       })
       createMd(page, blocks)
+    } catch (error) {
+      console.error(error)
+      continue
     }
-  } catch (error) {
-    console.error(error)
   }
 }
 
 function createMd(page, blocks) {
 	try {
-    file_name = page.properties.Filename.rich_text[0].plain_text + ".md"
+    file_name = temp_post_dir + "/" + page.properties.Filename.rich_text[0].plain_text + ".md"
+    console.log("Creating " + file_name)
+    prev_block_is_list = false
     addHeader(file_name, page.properties)
     addContent(file_name, blocks.results)
 
@@ -41,6 +51,9 @@ function createMd(page, blocks) {
     // const blockType = blocks.results[0]
     // console.log(blocks.results[0].paragraph.rich_text[0].plain_text)
 	} catch (error) {
+    if (!debug_mode) {
+      fs.unlink(file_name, appendErrorCallback)
+    }
     console.error(error)
 	}
 }
@@ -54,13 +67,15 @@ function addHeader(file_name, properties) {
   fs.appendFile(file_name, '\n+++\n\n', appendErrorCallback);
 }
 
-function addContent(file_name, blocks) {
+async function addContent(file_name, blocks) {
   for (let i = 0; i < blocks.length; i++) {
   // for (let i = 4; i>=0; i=-1) {
     try {
     console.log(blocks[i])
     console.log(blocks[i][blocks[i].type])
-    fs.appendFile(file_name, parseBlock(blocks[i]), appendErrorCallback);
+      const content = await Promise.resolve(parseBlock(blocks[i]))
+      console.log(content)
+    fs.appendFile(file_name, content, appendErrorCallback);
     } catch (error) {
       console.error(error)
       continue
@@ -69,16 +84,26 @@ function addContent(file_name, blocks) {
 }
 
 function parseBlock(block) {
+  // bulleted_list_item / numbered_list_item
+  if (block.type.endsWith("list_item")) {
+    prev_block_is_list = true
+    return getList(block, 0)
+  }
+
+  let line = prev_block_is_list ? "\n" : ""
+  prev_block_is_list = false
+
+  // heading_1 .. heading 6
   if (block.type.startsWith("heading")) {
-    return getHeading(block)
+    line += getHeading(block)
   }
-  if (block.type == "paragraph") {
-    return getParagraph(block.paragraph)
+  else if (block.type == "paragraph") {
+    line += getParagraph(block.paragraph)
   }
-  if (block.type == "image") {
-    return getImage(block.image)
+  else if (block.type == "image") {
+    line += getImage(block.image)
   }
-  return ""
+  return line
 }
 
 function getCategory(category) {
@@ -104,11 +129,15 @@ function getHeading(block) {
   const type = block.type
   const headingSize = parseInt(type[type.length - 1])
 console.log(headingSize)
-  return "#".repeat(headingSize) + " " + block[type].rich_text[0].plain_text + '\n\n'
+  return "#".repeat(headingSize) + " " + getLine(block[type]) + '\n\n'
 }
 
 function getParagraph(paragraph) {
-  return paragraph.rich_text.map(r => {
+  return getLine(paragraph) + "\n\n"
+}
+
+function getLine(obj) {
+  return obj.rich_text.map(r => {
     let s = r.plain_text
     if (r.href != null) {
       s = "[" + s + "](" + r.href + ")"
@@ -129,17 +158,34 @@ function getParagraph(paragraph) {
       s = "<del>" + s + "</del>"
     }
     return s;
-  }).join("") + "\n\n"
-    /*
-  for (let i = 0; i < paragraph.rich_text.length; i++) {
-    console.log(paragraph.rich_text[i].annotations)
-  }
-  return ''
-  */
+  }).join("")
 }
 
 function getImage(image) {
   return "![IMAGE](" + image.external.url + ")\n\n"
+}
+
+async function getList(block, layer) {
+  const type = block.type
+  const ordered = block.type.startsWith("numbered")
+  // indent: 4 for ordered list and 2 for unordered list
+  const prefix = " ".repeat(layer * (ordered ? 4 : 2)) + (ordered ? "1. " : "* ")
+  console.log(ordered)
+  let line = prefix + getLine(block[type])
+  if (block.has_children) {
+    console.log("hascccccccccccc")
+    const children = await notion.blocks.children.list({
+      block_id: block.id
+    })
+    console.log(children)
+    for (let i = 0; i < children.results.length; i++) {
+      line += "\n" + await getList(children.results[i], layer + 1)
+    }
+  }
+  // console.log(line)
+  // only one line break.
+  return line + "\n"
+
 }
 
 function appendErrorCallback(error) {
