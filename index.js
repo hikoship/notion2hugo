3216fe5c15b2538468afd39c41c87288a56fa129
hikoship <gao0927@gmail.com>
2023-03-24 03:02:54 -0400

    integrated hugo; bug fixes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-console.log('hgao')
+console.log('Starting the workflow')
+
+const path = require('path');
+
 const { Client } = require("@notionhq/client")
 require('dotenv').config()
 const fs = require('fs').promises;
+const { exec } = require('child_process');
 
-const debug_mode = true
+const debug_mode = false
 
 const notion = new Client({ auth: process.env.NOTION_KEY })
 const databaseId = process.env.NOTION_DATABASE_ID
@@ -15,6 +19,7 @@ let prev_block_is_list = false
 
 async function fetch() {
   const notion = new Client({ auth: process.env.NOTION_KEY })
+  console.log('Querying database')
   const database = await notion.databases.query({
     database_id: databaseId,
       filter: {
@@ -24,60 +29,60 @@ async function fetch() {
         }
       },
   })
+  let count = 0
   for (let i = 0; i < database.results.length; i++) {
- // for (let i = 0; i < 1; i++) {
     try {
       const page = database.results[i]
+      console.log("Processing article " + page.properties.Name.title[0].plain_text)
       const blocks = await notion.blocks.children.list({
         block_id: page.id
       })
-      console.log(page.properties.Name.title[0].plain_text)
-      // createMd(page, blocks)
+      createMd(page, blocks)
+      count++
     } catch (error) {
       console.error(error)
       continue
     }
   }
+  console.log(count + 'Markdown files generated')
+  //await exec('ls -l', (err, stdout, stderr) => {
+  await exec('. ' + process.env.HUGO_SCRIPT, execCallback);
+
 }
 
-function createMd(page, blocks) {
+async function createMd(page, blocks) {
 	try {
-    file_name = temp_post_dir + "/" + page.properties.Filename.rich_text[0].plain_text + ".md"
+    file_name = path.resolve(post_dir + "/" + page.properties.Filename.rich_text[0].plain_text + ".md")
     console.log("Creating " + file_name)
     prev_block_is_list = false
-    addHeader(file_name, page.properties)
+    await addHeader(file_name, page.properties).then(() => {
     addContent(file_name, blocks.results)
-
-    // console.log(page.properties.Name.title[0].plain_text)
-    // console.log(blocks)
-    // const blockType = blocks.results[0]
-    // console.log(blocks.results[0].paragraph.rich_text[0].plain_text)
+    })
+    console.log("Added header " + file_name)
+    console.log("Added content " + file_name)
 	} catch (error) {
     if (!debug_mode) {
-      fs.unlink(file_name, appendErrorCallback)
+      await fs.unlink(file_name, appendErrorCallback)
     }
     console.error(error)
 	}
 }
 
-function addHeader(file_name, properties) {
-  fs.writeFile(file_name, '+++\n\n', appendErrorCallback);
-  fs.appendFile(file_name, getCategory(properties.Category), appendErrorCallback);
-  fs.appendFile(file_name, getDate(properties.Date), appendErrorCallback);
-  fs.appendFile(file_name, getSummary(properties.Summary), appendErrorCallback);
-  fs.appendFile(file_name, getTitle(properties.Name), appendErrorCallback);
-  fs.appendFile(file_name, '\n+++\n\n', appendErrorCallback);
+async function addHeader(file_name, properties) {
+  await fs.writeFile(file_name, '+++\n\n', appendErrorCallback);
+  await fs.appendFile(file_name, getCategory(properties.Category), appendErrorCallback);
+  await fs.appendFile(file_name, getDate(properties.Date), appendErrorCallback);
+  await fs.appendFile(file_name, getSummary(properties.Summary), appendErrorCallback);
+  await fs.appendFile(file_name, getTitle(properties.Name), appendErrorCallback);
+  await fs.appendFile(file_name, '\n+++\n\n', appendErrorCallback);
 }
 
 async function addContent(file_name, blocks) {
   for (let i = 0; i < blocks.length; i++) {
   // for (let i = 4; i>=0; i=-1) {
     try {
-    console.log(blocks[i])
-    console.log(blocks[i][blocks[i].type])
-      const content = await Promise.resolve(parseBlock(blocks[i]))
-      console.log(content)
-    fs.appendFile(file_name, content, appendErrorCallback);
+      const content = await parseBlock(blocks[i])
+      await fs.appendFile(file_name, content, appendErrorCallback);
     } catch (error) {
       console.error(error)
       continue
@@ -85,7 +90,7 @@ async function addContent(file_name, blocks) {
   }
 }
 
-function parseBlock(block) {
+async function parseBlock(block) {
   // bulleted_list_item / numbered_list_item
   if (block.type.endsWith("list_item")) {
     prev_block_is_list = true
@@ -95,7 +100,7 @@ function parseBlock(block) {
   let line = prev_block_is_list ? "\n" : ""
   prev_block_is_list = false
 
-  // heading_1 .. heading 6
+  // heading_1 .. heading_3
   if (block.type.startsWith("heading")) {
     line += getHeading(block)
   }
@@ -129,8 +134,8 @@ function getTitle(name) {
 
 function getHeading(block) {
   const type = block.type
-  const headingSize = parseInt(type[type.length - 1])
-console.log(headingSize)
+  // Notion heading 1 = html heading 2
+  const headingSize = parseInt(type[type.length - 1]) + 1
   return "#".repeat(headingSize) + " " + getLine(block[type]) + '\n\n'
 }
 
@@ -172,29 +177,30 @@ async function getList(block, layer) {
   const ordered = block.type.startsWith("numbered")
   // indent: 4 for ordered list and 2 for unordered list
   const prefix = " ".repeat(layer * (ordered ? 4 : 2)) + (ordered ? "1. " : "* ")
-  console.log(ordered)
   let line = prefix + getLine(block[type])
   if (block.has_children) {
-    console.log("hascccccccccccc")
     const children = await notion.blocks.children.list({
       block_id: block.id
     })
-    console.log(children)
     for (let i = 0; i < children.results.length; i++) {
-      line += "\n" + await getList(children.results[i], layer + 1)
+      line += "\n" + getList(children.results[i], layer + 1)
     }
   }
-  // console.log(line)
   // only one line break.
   return line + "\n"
-
 }
 
 function appendErrorCallback(error) {
-      if (error) throw error;
-    }
+  if (error) throw error;
+}
 
+function execCallback(error, stdout, stderr) {
+  if (error) {
+    console.error(`Error executing command: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+}
 
-
-console.log('hgao')
 fetch()
